1b5c77d0ededd9c95bf5e96407dcd3cb1f7f3381
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-04-11 17:36:39 +0200

    access token configuration & login page flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import './App.css';
 import {Stack, Button, Box} from "@mui/material"
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './pages/Login';
+import { getAccessToken } from './utils/getAccessToken';
+import { getAccessTokenFromStorage } from './utils/getAccessTokenFromStorage';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App({spotifyApi}) {
-	console.log(spotifyApi);
+	const [token, setToken] = useState(getAccessTokenFromStorage())
+
+	useEffect(() => {
+		const accessToken = getAccessTokenFromStorage() || getAccessToken()
+		
+		if(accessToken) {
+			setToken(accessToken);
+			sessionStorage.setItem("spotifyToken", accessToken);
+			window.location.hash = ""
+		}
+	}), []
+
 	return (
 		<Box className="App">
-			<Dashboard spotifyApi={spotifyApi}/>
+			{token ? (
+				<Dashboard spotifyApi={spotifyApi} />
+			) : (
+				<Routes>
+					<Route path="*" element={<Login />}/>
+				</Routes>
+			
+			)}
 		</Box>
 	);
 }
