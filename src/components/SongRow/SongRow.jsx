e78dadb789aa9710985a2034ac0d83426ed0537f
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-04-19 16:28:37 +0200

    finished songrow component with skeleton loaders

--- src/components/SongRow/SongRow.jsx
+++ src/components/SongRow/SongRow.jsx
@@ -1,23 +1,38 @@
-import { Box, Avatar, Grid, Typography } from '@mui/material';
-const SongRow = ({images, title, artist, album, duration, i, loading}) => {
-    const image = images?.length > 0 ? images[0].url : null
+import { Box, Avatar, Grid, Typography, Skeleton } from '@mui/material';
+const SongRow = ({ images, title, artist, album, duration, i, loading }) => {
+	const image = images?.length > 0 ? images[0].url : null;
 	return (
-		<Grid container px={2} p={1} sx={{ width: '100%', color: 'text.secondary', fontSize: 14, cursor: "pointer", "&:hover": {backgroundColor: "#ffffff10"} }}>
+		<Grid
+			container
+			px={2}
+			p={1}
+			sx={{
+				width: '100%',
+				color: 'text.secondary',
+				fontSize: 14,
+				cursor: 'pointer',
+				'&:hover': { backgroundColor: '#ffffff10' }
+			}}
+		>
 			<Grid sx={{ width: 35, display: 'flex', alignItems: 'center' }} item>
 				{i + 1}
 			</Grid>
-			<Grid item sx={{ flex: 1, display: 'flex', alignItems: 'center', gap: 2}}>
-                <Avatar src={image} alt={null} variant='square'/>
-                <Box>
-                    <Typography sx={{fontSize: 16, color: "text.primary"}}>{title}</Typography>
-                    <Typography sx={{fontSize: 16, color: "text.secondary"}}>{artist}</Typography>
-                </Box>
+			<Grid item sx={{ flex: 1, display: 'flex', alignItems: 'center', gap: 2 }}>
+				{loading ? <Skeleton variant="rectangular" width={40} height={40}/> : <Avatar src={image} alt={null} variant="square" />}
+				<Box>
+					<Typography sx={{ fontSize: 16, color: 'text.primary' }}>
+                        {loading ? <Skeleton variant='text' width={130} height={24}/> : title}
+                        </Typography>
+					<Typography sx={{ fontSize: 16, color: 'text.secondary' }}>
+                        {loading ? <Skeleton variant='text' width={50} height={18}/> : artist} 
+                    </Typography>
+				</Box>
 			</Grid>
-			<Grid xs={3} item sx={{ display: { xs: 'none', md: 'flex' }, alignItems: "center" }}>
-				{album}
+			<Grid xs={3} item sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
+            {loading ? <Skeleton variant='text' width={50} height={24}/> : album}
 			</Grid>
 			<Grid xs={3} item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-				{duration}
+            {loading ? <Skeleton variant='text' width={50} height={18}/> : duration}
 			</Grid>
 		</Grid>
 	);
