29e576db13f461a18a05dc347c5134794a6510fd
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-04-22 16:44:14 +0200

    finished player component styling

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,14 +3,19 @@ import { Box, Grid, Typography, Avatar } from '@mui/material';
 const Player = ({ spotifyApi }) => {
 	return (
 		<Box>
-			<Grid container>
+			<Grid container px={3} sx={{backgroundColor: "background.paper", height: 100, cursor: {xs: "pointer", md: "auto"}, width: "100%", borderTop: "1px solid #292929"}}>
 				<Grid xs={12} md={4} items sx={{display: "flex", alignItems: "center", justifyContent: "flex-start"}}>
-					Bild, Title, Artist
+					<Avatar src={null} alt={null} variant="square" sx={{width: 56, height: 56, marginRight: 2}}/>
+                    <Box>
+                        <Typography sx={{color: "text.primary", fontSize: 14}}>Suh dude</Typography>
+                        <Typography sx={{color: "text.secondary", fontSize: 10}}>Jag</Typography>
+                    </Box>
 				</Grid>
 				<Grid
 					sx={{ display: { xs: 'none', md: 'flex' }, 
-                    justifyContent: 'flex-end', 
-                    alignItems: 'center' }} md={4} items>
+                    justifyContent: 'center', 
+                    alignItems: 'center' }} 
+                    md={4} items>
 					Playknapp
 				</Grid>
 				<Grid xs={6} md={4} items sx={{display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
